b09a85381c82dce0be36c6feba0a0f2b95087f12
romanhess98 <roman@schmuedderich.de>
2023-01-29 08:01:42 +0100

    implement storage of classifier

--- user_classification_transformer/run_training.sh
+++ user_classification_transformer/run_training.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=4
-python3 training.py --train_ds=df_random_nd_c_train --val_ds=df_random_nd_c_val --test_ds=df_random_nd_c_test --mode=train
+python3 training.py --train_ds=df_random_nd_c_train --val_ds=df_random_nd_c_val --test_ds=df_random_nd_c_test --mode=test
